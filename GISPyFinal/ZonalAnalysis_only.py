faeef877db520377b13e90be274239bcb7eb00cf
LaithL24 <119709399+LaithL24@users.noreply.github.com>
2022-12-07 23:04:19 -0700

    Update ZonalAnalysis_only.py

--- GISPyFinal/ZonalAnalysis_only.py
+++ GISPyFinal/ZonalAnalysis_only.py
@@ -3,7 +3,6 @@
 
 # In[36]:
 
-
 import pandas as pd
 import geopandas as gpd
 import rasterio
@@ -12,13 +11,11 @@ from rasterstats import zonal_stats
 
 # In[43]:
 
-
 import warnings
 warnings.filterwarnings("ignore")
 
 
 # In[44]:
-
 
 merge = gpd.read_file(".\Data\merged\Def_Mis_merged.shp") # open merged sidewalk shapefile
 rpath = "./Data/Raster/LP_Distance_Raster_5ft.tif"  # path to distance raster from arcpro
@@ -26,12 +23,10 @@ rpath = "./Data/Raster/LP_Distance_Raster_5ft.tif"  # path to distance raster fr
 
 # In[45]:
 
-
 stats = zonal_stats(merge['geometry'], rpath, stats= "mean")
 
 
 # In[46]:
-
 
 means = []
 for stat in stats:
@@ -40,18 +35,14 @@ for stat in stats:
 
 # In[40]:
 
-
 merge['AvgDistLP'] = means
 
 
 # In[41]:
-
 
 merge.head() # can see that column was added
 
 
 # In[42]:
 
-
 merge.to_file("./Data/Raster/MeanDist_sidewalks.shp")
-
